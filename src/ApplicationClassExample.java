529166b81bbd5124e0de3af4f59f2282dc0da440
Federica Bardini <federica.bardini03@gmail.com>
2018-09-23 23:14:10 +0200

    Added setters and constructors

--- src/ApplicationClassExample.java
+++ src/ApplicationClassExample.java
@@ -3,9 +3,27 @@ import java.util.Scanner;
 class Cats {
 
     //Definition of instance variables (data or state)
-    String name;
-    String color;
-    int age;
+    private String name;
+    private String color;
+    private int age;
+
+    //adding constructor methods
+    public Cats() {
+        /*name = "Bellatrix";
+        color = "black";
+        age = 5;*/
+        this("Bellatrix", "black", 5);
+        System.out.println("Calling constructor n 1!");
+
+    }
+
+
+    public Cats(String name, String color, int age){
+        this.name = name;
+        this.color = color;
+        this.age = age;
+        System.out.println("Calling constructor n 2!");
+    }
 
     //define a new method
     void meow() {
@@ -15,7 +33,6 @@ class Cats {
     //define a method with a return value
     int getMyHumanAge() {
         int humanAge = age*2;
-
         return humanAge;
     }
 
@@ -31,18 +48,58 @@ class Cats {
     String getColor() {
         return color;
     }
+
+    //define a method with input parameters
+    void catSpeak(String someText) {
+        System.out.println(someText);
+    }
+
+    //define setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
 
 
 class People {
 
-    String name;
-    int age;
-    String occupation;
-    String haircolor;
+    private String name;
+    private int age;
+    private String occupation;
+    private String haircolor;
 
     void tellMeSomethingAboutYou() {
         System.out.println("My name is " + name + " and my hair are " + haircolor + ". \nI'm " + age + " years old and I'm a " + occupation);
+    }
+
+    void move(String direction, int km){
+        System.out.println("I'm able to move " + direction + " for " + km + " km a day");
+    }
+
+    //define setters
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setHaircolor(String haircolor) {
+        this.haircolor = haircolor;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
     }
 }
 
@@ -54,17 +111,23 @@ public class ApplicationClassExample {
 
         //new object kitty
         Cats kitty = new Cats();
+        Cats kitty2 = new Cats("JJ","white",1);
 
-        kitty.age = 3;
-        kitty.color = "grey";
-        kitty.name = "Narcissa";
+        //kitty.setAge(3);
+       // kitty.setColor("grey");
+        //kitty.setName("Narcissa");
+
+        String meowing = "Lots of meow for you";
 
         People misterX = new People();
 
-        misterX.haircolor = "blonde";
-        misterX.age = 54;
-        misterX.name = "No one";
-        misterX.occupation = "Misterious man - maybe a thief";
+        misterX.setAge(54);
+        misterX.setHaircolor("blonde");
+        misterX.setName("No one");
+        misterX.setOccupation("Misterious man - maybe a thief");
+
+        String manDirection = "North";
+        int kmnum = 35;
 
         Scanner inputObject = new Scanner(System.in);
 
@@ -72,32 +135,19 @@ public class ApplicationClassExample {
         String newLine = inputObject.nextLine();
         System.out.println("You have choosen: "+ newLine);
 
-        /*if (newLine.equals("Cats")) {
-            System.out.println(kitty.name);
-            System.out.println(kitty.age);
-            System.out.println(kitty.color);
-        }
-        else if (newLine.equals("People")) {
-            System.out.println(misterX.name);
-            System.out.println(misterX.age);
-            System.out.println(misterX.haircolor);
-            System.out.println(misterX.occupation);
-        }
-
-        else {
-            System.out.println("You have inserted an unknown option: please choose between 'Cats' or 'People'");
-        }*/
-
         switch (newLine) {
             case "Cats":
-                //kitty.meow();
+                kitty.meow();
+                kitty2.meow();
                 int humAge = kitty.getMyHumanAge();
                 int catAge = kitty.getAge();
                 System.out.println("My human age is " + humAge + " but my real age is " + catAge);
+                kitty.catSpeak(meowing);
             break;
 
             case "People":
                 misterX.tellMeSomethingAboutYou();
+                misterX.move(manDirection, kmnum);
             break;
             default:
                 System.out.println("You have inserted an unknown option: please choose between 'Cats' or 'People'");
