22c9846f87fb67ec444a3c10165e3784d6e5bf54
Federica Bardini <federica.bardini03@gmail.com>
2018-10-10 00:39:34 +0200

    Simple exercise taken from "https://exercism.io/tracks/java/exercises/two-fer/"

---
+++ src/Exercism/two/fer/Appexec.java
@@ -0,0 +1,15 @@
+package Exercism.two.fer;
+
+public class Appexec {
+
+    public static void main(String[] args) {
+
+        TwoFer ex1 = new TwoFer();
+
+        String nullString = null;
+
+        ex1.printName("Matteo");
+        ex1.printName(nullString);
+
+    }
+}
---
+++ src/Exercism/two/fer/TwoFer.java
@@ -0,0 +1,12 @@
+package Exercism.two.fer;
+
+public class TwoFer {
+
+    public void printName(String name) {
+        if (name == null) name = "You";
+
+        System.out.printf("One for %s, one for me.\n", name);
+    }
+
+
+}
